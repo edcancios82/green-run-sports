d40405048d5dcfda38f1e7485463652e8bf7419c
edcancios82 <58710267+edcancios82@users.noreply.github.com>
2022-01-06 21:03:21 -0300

    Added axios request

---
+++ src/api/axiosRequest.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const axiosGet = () => {
+  return axios
+    .get("/user?ID=12345")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+};
